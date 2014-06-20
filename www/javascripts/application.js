ecdb3fd45efbc53e352f83ac0253172fafd9ec45
AmirMgh <temp@amir.com>
2014-06-20 01:21:14 -0700

    C#1: First Commit, Steroids created

---
+++ www/javascripts/application.js
@@ -0,0 +1,5 @@
+// Display the native navigation bar with the title "LifeLy"
+steroids.view.navigationBar.show("LifeLy");
+
+// Set the WebView background color to white (effective on iOS only)
+steroids.view.setBackgroundColor("#FFFFFF");
